d3ef74887514673bf7c5f4dab747aaf796c3360e
rizkyperdananst <riskiperdana345@gmail.com>
2023-09-05 20:45:37 +0700

    Add Pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-slate-50">
+    <nav className="bg-slate-50 border-b-2 border-slate-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
